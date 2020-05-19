0800de2d01c0ae4fca260270b1a17841f4c00524
梅宮 <euthanasia045@gmail.com>
2020-05-19 22:39:33 +0900

    🛠 groovyのアプリを終了します・・・

--- app.groovy
+++ app.groovy
@@ -5,7 +5,17 @@ class App {
     @RequestMapping("/")
     @ResponseBody
     def home(ModelAndView mav) {
-        mav.setViewName("home")
+        mav.setViewName("input")
+        mav.addObject("msg", "please write your name!!")
+        mav
+    }
+
+    @RequestMapping(value="/send", method=RequestMethod.POST)
+    @ResponseBody
+    def send(@RequestParam("text1")String str, ModelAndView mav) {
+        mav.setViewName("input")
+        mav.addObject("msg", "hello, " + str)
+        mav.addObject("value", str)
         mav
     }
 }
